99a72b23abf40ba85d843e952cfc809be3b144a4
Rahul <rahulsingh@enterprisemonkley.com>
2025-11-15 18:18:20 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./routes/user.routes.js";
 
 const app = express();
 
@@ -15,5 +16,8 @@ app.use(express.json({ limit: "16kb" })); // json body limit set to 16kb
 app.use(express.urlencoded({ limit: "16kb", extended: true })); // to parse URL-encoded bodies like rahul.com?name=singh&age=30, extended: true to parse nested objects
 app.use(express.static("public")); // to serve static files from 'public' directory
 app.use(cookieParser()); // to parse cookies from request headers
+
+// Define a simple route for testing
+app.use("/api/v1/users", router);
 
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // This is the second method to create asyncHandler middleware
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
